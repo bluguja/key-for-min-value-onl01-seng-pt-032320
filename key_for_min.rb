148bf6497a0653982af79b1cf98793b999637eb1
Bala Luguja <bluguja@gmail.com>
2020-03-30 22:29:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,13 +8,13 @@ def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil
   
-  name_hash.each do |key,value|
+  name_hash.each { |key,value|}
      
         if lowest_value == 0 || value < lowest_value
       lowest_value = value 
       lowest_key = key
     end
-  end
+  #end
   lowest_key
 end
 
