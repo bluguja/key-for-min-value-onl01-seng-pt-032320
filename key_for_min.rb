277fb8efcca100fb0f7a1a97fa969ac6ee616fa8
Bala Luguja <bluguja@gmail.com>
2020-03-30 22:27:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 #require 'pry'
 
-def key_for_min_val(name_hash)
+def key_for_min_value(name_hash)
 
-def key_for_min_val(name_hash)
-  lowest_val = 0
+def key_for_min_value(name_hash)
+  lowest_value = 0
   lowest_key = nil
   
-  name_hash.each do |key,val|
+  name_hash.each do |key,value|
      
-        if lowest_val == 0 || val < lowest_val
-      lowest_val = val 
+        if lowest_value == 0 || val < lowest_value
+      lowest_value = value 
       lowest_key = key
     end
   end
@@ -19,7 +19,7 @@ def key_for_min_val(name_hash)
 end
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}
-key_for_min_val(ikea)
+key_for_min_value(ikea)
 
 #binding.pry
 end
