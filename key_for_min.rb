f4d6c7b2e1717a11c0fb8f2aa59de819c4221d35
Bala Luguja <bluguja@gmail.com>
2020-03-30 22:17:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
 
 def key_for_min_value(name_hash)
@@ -8,10 +8,8 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   
   name_hash.each do |key,value|
-    #if the lowest_value is in it's intial state  
-    #or if the value of the current key is less than the lowest_value
-    #then: set the lowest_value to value and lowest_key to key
-    if lowest_value == 0 || value < lowest_value
+     
+        if lowest_value == 0 || value < lowest_value
       lowest_value = value 
       lowest_key = key
     end
@@ -22,15 +20,5 @@ end
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 key_for_min_value(ikea)
 
-
-
-
-
-#veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
-#key_for_min_value(veggies)
-
-
-
-
-
+binding.pry
 end
