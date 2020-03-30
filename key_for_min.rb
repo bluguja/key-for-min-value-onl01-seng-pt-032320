39ab7e3a7fdd929e2f2789562a3e5c6c46756753
Bala Luguja <bluguja@gmail.com>
2020-03-30 22:23:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 #require 'pry'
-#def key_for_min_value(name_hash)
+
+def key_for_min_value(name_hash)
 
 def key_for_min_value(name_hash)
   lowest_value = 0
@@ -17,7 +18,7 @@ def key_for_min_value(name_hash)
   lowest_key
 end
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
+#ikea = {:chair => 25, :table => 85, :mattress => 450}
 key_for_min_value(ikea)
 
 #binding.pry
